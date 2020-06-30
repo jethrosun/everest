cb392229bf3ec85bccb845e304ae4e930bab18c1
Tahina Ramananandro <taramana@microsoft.com>
2020-06-29 20:56:28 -0700

    update

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=e6d54139a37dd109ccade77a435bd41f6a9a779a
+hashes[FStar]=fe66f07e5a41bca531c0616b090a60fe1334cbe4
 branches[FStar]=_c_layeff
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
@@ -8,7 +8,7 @@ hashes[hacl-star]=32ba189487f3b08d9e8e0a29c92307f3444f7e74
 branches[hacl-star]=master
 hashes[kremlin]=0f08f1243331ffc62ba3f1b8d8b06e4de8479943
 branches[kremlin]=master
-hashes[mitls-fstar]=76f6c4abfc469a25a4e4a2b8657ed4f40b5f1405
+hashes[mitls-fstar]=a1ec49081c934ced9201a5226694e412d0688979
 branches[mitls-fstar]=_taramana_layeff
-hashes[quackyducky]=a5b47fd923954f6b0924aa4c968d243a94a9b6d3
+hashes[quackyducky]=032741bf5fcf95c2951de0c43bedb628132c33c3
 branches[quackyducky]=taramana_layeff
