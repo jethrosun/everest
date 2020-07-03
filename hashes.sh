4dfcff172d8b77d97b4e3b2fe1ccdfc1995ba3a4
Tahina Ramananandro <taramana@microsoft.com>
2020-07-02 22:18:19 -0700

    update: some extraction works

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=c83f4aeefd916f39fb898f2d57f8dd51ea262c0c
+hashes[FStar]=0003fe3e6f4c5871d4ada473622fde2e665bf891
 branches[FStar]=_c_layeff
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
@@ -8,7 +8,7 @@ hashes[hacl-star]=9cd7e909083a04452f7ef614fae48aa45db88dc2
 branches[hacl-star]=master
 hashes[kremlin]=0f08f1243331ffc62ba3f1b8d8b06e4de8479943
 branches[kremlin]=master
-hashes[mitls-fstar]=fcb83e9b9f50d1c50d7180e56169436955db2f8d
+hashes[mitls-fstar]=194b41f944ca07c7243d8254ab286764fce93cef
 branches[mitls-fstar]=_taramana_layeff
-hashes[quackyducky]=19774bdee7699c79024d5cde5127bebc739017de
+hashes[quackyducky]=7513259e7bbc4ad3ec97b0a339c13779c48f8708
 branches[quackyducky]=taramana_layeff
