af1caa1e0bb596a0afff698935c955df2a10470e
Tahina Ramananandro <taramana@microsoft.com>
2020-07-08 00:04:19 -0700

    update: complete write_final_extensions

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=0003fe3e6f4c5871d4ada473622fde2e665bf891
+hashes[FStar]=ce4207f95d7727d9a3ce8687c4215295854f784d
 branches[FStar]=_c_layeff
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=9cd7e909083a04452f7ef614fae48aa45db88dc2
+hashes[hacl-star]=e78c07be155b4fcdb1ade613231729d96a51bf43
 branches[hacl-star]=master
 hashes[kremlin]=0f08f1243331ffc62ba3f1b8d8b06e4de8479943
 branches[kremlin]=master
-hashes[mitls-fstar]=194b41f944ca07c7243d8254ab286764fce93cef
+hashes[mitls-fstar]=a18e414d398b397045d10ffe4806e8af0422a84d
 branches[mitls-fstar]=_taramana_layeff
-hashes[quackyducky]=7513259e7bbc4ad3ec97b0a339c13779c48f8708
+hashes[quackyducky]=f9432c39c9e400c2e98d2cf2ef052fe70509e993
 branches[quackyducky]=taramana_layeff
