1c222785d1ca42a6c73871a8756ef712e77c8c4a
Tahina Ramananandro <taramana@microsoft.com>
2020-07-09 21:06:25 -0700

    update: POPL 2021 submission

--- hashes.sh
+++ hashes.sh
@@ -1,6 +1,6 @@
 declare -A hashes
 declare -A branches
-hashes[FStar]=ce4207f95d7727d9a3ce8687c4215295854f784d
+hashes[FStar]=313fbe1d54d870afd43627d4be64b7f692fcf3fd
 branches[FStar]=_c_layeff
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
@@ -8,7 +8,7 @@ hashes[hacl-star]=e78c07be155b4fcdb1ade613231729d96a51bf43
 branches[hacl-star]=master
 hashes[kremlin]=0f08f1243331ffc62ba3f1b8d8b06e4de8479943
 branches[kremlin]=master
-hashes[mitls-fstar]=a18e414d398b397045d10ffe4806e8af0422a84d
+hashes[mitls-fstar]=9323ce953ca00c7bb842421fb3f7a4c17a3c14d9
 branches[mitls-fstar]=_taramana_layeff
-hashes[quackyducky]=f9432c39c9e400c2e98d2cf2ef052fe70509e993
+hashes[quackyducky]=d8f4c27aa9640a1da5951469c8100744d6404417
 branches[quackyducky]=taramana_layeff
