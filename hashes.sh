53349676aef83c7529132e1c9efb8a849a831df7
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2021-07-02 13:57:06 -0700

    try

--- hashes.sh
+++ hashes.sh
@@ -4,11 +4,12 @@ hashes[FStar]=045a8e12422f9310212da7f30d839ebb2740adb0
 branches[FStar]=master
 hashes[hacl-star]=fe6be9a82ebcd57eafab94c1fff749bd15947758
 branches[hacl-star]=master
-hashes[kremlin]=986bd298eecedc96278e1ba712fadd7c5f7d276b
-branches[kremlin]=master
+hashes[kremlin]=b27a36db127f3cb763b450ee8f5e46e5130d6887
+branches[kremlin]=protz_colored_region_fix
 hashes[mitls-fstar]=9d33728052f805801a3bc359d950bb1b178db572
 branches[mitls-fstar]=master
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
 hashes[quackyducky]=63b27f0cc90bb73be518f28505efbc7f6f5ffc7c
 branches[quackyducky]=master
+
